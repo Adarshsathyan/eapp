0fd744d0ceaf3afdc1e1d7ccfb24f48c9806dbc7
Adarshsathyan <adarshsathyan2000@gmail.com>
2020-09-15 10:18:42 +0530

    Created or modified models functions to get cart total and quantity and created views for aunticated and non authentictaed user

--- eapp/core/views.py
+++ eapp/core/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Products,Cart
+from .models import *
 # Create your views here.
 def index(request):
     product = Products.objects.all()
@@ -13,9 +13,16 @@ def wlist(request):
     return render(request,'core/product_list.html')
 
 def cart(request):
-    items = Cart.objects.all()
-    product = Products.objects.all()
-    return render(request,'core/cart.html',{'items':items,'product':product})
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.cart_set.all()
+    else:
+
+        items = []
+        order ={' get_cart_total':0 , 'get_cart_items':0}
+    return render(request,'core/cart.html',{'items':items,'order':order})
 
 def checkout(request):
     return render(request,'core/checkout.html')
